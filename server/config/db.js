54bf2d9ee0e0c5bb976bb5e0678371aa56a4e6dc
BixbyIp <cip30@my.centennialcollege.ca>
2023-04-03 09:13:09 +0800

    02042023091305

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/contact_db"
-    "URI": "mongodb+srv://cip30:M4KNI1uRreVSK43T@cluster0.uuwhj4d.mongodb.net/test"
+    "URI": "mongodb+srv://cip30:M4KNI1uRreVSK43T@cluster0.uuwhj4d.mongodb.net/surveyDB"
 
 }
